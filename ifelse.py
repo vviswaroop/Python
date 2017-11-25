04c7ab9917022af6a9440d200f46af0c17c2706c
viswa <vvadlamu@kent.edu>
2017-11-24 22:04:52 -0500

    sample basics

---
+++ ifelse.py
@@ -0,0 +1,5 @@
+password = 'swordfish'
+if password == 'swordfish':
+    print('Access Granted')
+else:
+    print('Wrong Password')
---
+++ sample.py
@@ -0,0 +1,15 @@
+# This is my first program
+
+print('Hello World!')
+
+print('What is your age?') #ask their age
+myAge = input()
+print('you will be ' + str(int(myAge) + 1) + ' in a year')
+
+print('What is your name?') # ask their name
+name = input()
+print('Its good to meet you, ' + ame)
+
+print('The length of your name is:')
+
+print(len(myName))
